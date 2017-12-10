5579fd197cb48a0a733679400571a32ddf98f310
daniel <daniel@daniel>
2017-12-10 18:28:19 +0100

    bicing2

--- src/org/escoladeltreball/bicing/Principal.java
+++ src/org/escoladeltreball/bicing/Principal.java
@@ -18,7 +18,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 /**
- * @author iaw39397876
+ * @author iaw26509397
  *
  */
 public class Principal {
